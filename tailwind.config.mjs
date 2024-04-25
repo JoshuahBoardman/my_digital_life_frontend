3f366dc36ab557636160ad4b61478240ed1c0537
Joshuah Boardman <joshuahboardman@gmail.com>
2024-04-25 17:30:44 -0500

    Add: custom colors, custom shadow, and styles for nav_button and terminal_window component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+        colors: {
+            'ebony-clay': '#232538',
+            'tuna': '#313345',
+            'sundown': '#FFB3B2',
+            'madang': '#BBF3B5',
+            'texas': '#FBF0A3',
+            'periwinkle-gray': '#C4C5E7',
+            'santas-gray': '#9397B0',
+            'mischka': '#DCDCE6',
+        },
+        boxShadow: {
+           'neo-brute': '10px 10px 5px 0px rgba(0,0,0,0.75)', 
+        },
 		extend: {},
 	},
 	plugins: [],
