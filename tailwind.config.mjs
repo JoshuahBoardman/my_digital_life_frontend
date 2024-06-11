0dd2cda66bf8c8983571698ad1649c13c5248f2a
Joshuah Boardman <joshuahboardman@gmail.com>
2024-06-11 13:29:16 -0500

    Refactor: tweek styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    /*Sytle NOTE: elemtns outside the terminal should have no outline, elements inside should have an outline*/
 	theme: {
         colors: {
             'ebony-clay': '#232538',
@@ -16,11 +17,20 @@ export default {
             'neo-brute': '8px 8px 4px 0px rgba(0,0,0,0.2)',
             'inset': 'inset 0px 2px 6px 0px rgba(0, 0, 0, 0.5)',
         },
+        borderRadius: {
+            regular: "0.5rem"
+        },
+        /*TODO: border radius .5rem*/
+        /*TODO: font size should have small, medium, and large*/
+        /*TODO: padding should have 2-3 padding sizes*/
+        /*TODO: gap should have 2ish gap sizes*/
+        /*TODO: border should have an in terminal border and a 0 border for out of terminal elements*/
 		extend: {},
 	},
     daisyui: {
         themes: [
           {
+            /* TODO: rethink color pallet a bit */
             vaporWaveFantasyDark: {
                 "primary": "#C4C5E7",
                 "primary-content": "#232538",
