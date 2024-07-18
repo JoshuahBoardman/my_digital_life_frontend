8ad0c8881fad307140c7c108e3e4bba5339f546c
Joshuah Boardman <joshuahboardman@gmail.com>
2024-07-17 19:26:47 -0500

    Update: add routes page and connected routes to nav links

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+
+
+export const routes = {
+    home: "/",
+    gitHub: "https://github.com/JoshuahBoardman",
+    blog: "/blogs/",
+    portfolio: "/portfolio/",
+    aboutMe: "/about_me/",
+    resume: "/", 
+    contact: "/"
+};
